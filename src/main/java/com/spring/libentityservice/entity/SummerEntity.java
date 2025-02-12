edab3fb340016e0d06de20dc3a481e36dadcc223
indumathy <indumathy.s@coherent.in>
2025-02-12 15:08:49 +0530

    added new repo and entity

---
+++ src/main/java/com/spring/libentityservice/entity/SummerEntity.java
@@ -0,0 +1,48 @@
+
+package com.spring.libentityservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "summer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SummerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+
+//    @OneToMany(mappedBy = "summer", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+//    @JsonManagedReference
+//    private List<Winter> winterList;
+
+}
