2ba35a5f94a8e8566e526541917e01108e28c805
indumathy <indumathy.s@coherent.in>
2025-02-14 17:46:17 +0530

    Update AuditsRepository.java

--- src/main/java/com/spring/libentityservice/repository/AuditsRepository.java
+++ src/main/java/com/spring/libentityservice/repository/AuditsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuditsRepository extends JpaRepository<AuditsEntity, Long> {
+public interface AuditsRepository extends JpaRepository<Audits, Long> {
 
 }
